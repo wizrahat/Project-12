cdb175de4ce2322cb88efbf3b3bea5baaef3f77e
rahat <rakibulalamrahat24@gmail.com>
2026-03-10 23:52:33 +0600

    fix:fixed build issues

--- app/dashboard/courses/page.jsx
+++ app/dashboard/courses/page.jsx
@@ -1,8 +1,11 @@
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
-import { getInstructorDashboardData, COURSE_DATA } from "@/lib/dashboard-helper";
+import {
+  getInstructorDashboardData,
+  COURSE_DATA,
+} from "@/lib/dashboard-helper";
 
-
+export const dynamic = "force-dynamic";
 const CoursesPage = async () => {
   const courses = await getInstructorDashboardData(COURSE_DATA);
 
